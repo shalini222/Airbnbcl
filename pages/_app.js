94608120b2742d9f22718a114b2426601cb0403d
Shalini Dutta <Shaliniduttaofficial@gmail.com>
2021-08-14 02:05:25 +0530

    modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,20 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
-import ProgressBar from "@badrap/bar-of-progress";
+import ProgressBar from "@badrap/bar-of-progress/dist";
+import React from "react";
 import Router from "next/router";
-
 const progress = new ProgressBar({
   size: 4,
-  color: "#FD5B61",
+  color: "#FE595E",
   className: "z-50",
   delay: 100,
 });
-
 Router.events.on("routeChangeStart", progress.start);
-
-Router.events.on("routeChangeEnd", progress.finish);
-
+Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
 export default MyApp;
 
 // pk.eyJ1Ijoic2hhbGluaTIyMiIsImEiOiJja3MyeGR0b2cxYmR1Mndwam1ic2xoNnA5In0.cTmvI_kqZAKUymWWwfIOBw
